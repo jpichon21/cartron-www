ed5dae963fb07a96421d92ecce9a59558f037f9f
Kalizo <matthieugodin@live.fr>
2018-08-09 12:04:18 +0200

    Fix .gitignore

--- src/Site/AdminBundle/SiteAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Site\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SiteAdminBundle extends Bundle
-{
-}
--- src/Site/CartronBundle/SiteCartronBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Site\CartronBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SiteCartronBundle extends Bundle
-{
-}
