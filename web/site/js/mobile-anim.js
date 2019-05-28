3917204a344a47c6b7a6ea5ca5ff5ba0ac386e52
jpichon21 <juliencfpichon@gmail.com>
2019-05-28 12:28:45 +0200

    correctif

--- web/site/js/mobile-anim.js
+++ web/site/js/mobile-anim.js
@@ -8,7 +8,7 @@ if (screen.width <= 568) {
         $('.savoir-faire-creation .retour').css("opacity", "0");
         $('.savoir-faire-creation .link-creation-bt').css("opacity", "0");
         $('.link-liqueur, .link-eau-vie-home').css("opacity", "0");
-        $('.savoir-faire-creation img').css("left", "-372px");
+        $('.savoir-faire-creation img').css("left", "-300px");
         setTimeout(function(){moveImgSlow();}, 200);
 
         ///variables
@@ -18,32 +18,32 @@ if (screen.width <= 568) {
 
             //animation bg
             function moveImgSlow(){
-                $('.savoir-faire-creation img').css({"left" : "-300px", "transition" : "2s ease-in"});
+                $('.savoir-faire-creation img').css({"left" : "-300px", "transition" : "2s ease"});
                 setTimeout(function(){moveImgBlur();}, 1800);
             };
 
             function moveImgBlur(){
-                $('.savoir-faire-creation img').css({"left" : "0px", "transition" : "2s ease-in"});
+                $('.savoir-faire-creation img').css({"left" : "0px", "transition" : "2s ease"});
                 setTimeout(function(){showTitleContent();}, 600);
             };
 
             function showTitleContent(){
                 $('.savoir-faire-creation h2, .savoir-faire-creation .retour').css("opacity", "1");
-                $('.savoir-faire-creation h2').css("transition", "2s ease-in");
-                $('.savoir-faire-creation .retour').css("transition", "3s ease-in");
+                $('.savoir-faire-creation h2').css("transition", "2s ease");
+                $('.savoir-faire-creation .retour').css("transition", "3s ease");
                 setTimeout(function(){ShowTextContent(Step, ShowTimer, i);}, 600);
                 setTimeout(function(){ShowFooterMenu(Step, ShowTimer, i);}, 1800);
             }
 
             function ShowTextContent(Step, ShowTimer, i){
                     i = i + 1;
-                    $('.link-liqueur, .link-eau-vie-home').css({"opacity" : "1", "transition" : "2s ease-in" });
-                    $('.desc-creation p:nth-child('+ i + ')').css({"opacity" : "1", "transition" : "2s ease-in"});
+                    $('.link-liqueur, .link-eau-vie-home').css({"opacity" : "1", "transition" : "2s ease" });
+                    $('.desc-creation p:nth-child('+ i + ')').css({"opacity" : "1", "transition" : "2s ease"});
                     setTimeout(function(){ShowTextContent(ShowTimer, Step, i);}, ShowTimer);
             };
 
             function ShowFooterMenu(){
-                $('.savoir-faire-creation .link-creation-bt').css({"opacity" : "1", "transition" : "2s ease-in"});
+                $('.savoir-faire-creation .link-creation-bt').css({"opacity" : "1", "transition" : "2s ease"});
             }
 
     });
