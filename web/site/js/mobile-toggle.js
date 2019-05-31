eccb5555b5d89c598943c19dfb785e8fa37d3f68
jpichon21 <juliencfpichon@gmail.com>
2019-05-31 17:04:42 +0200

    correctif

--- web/site/js/mobile-toggle.js
+++ web/site/js/mobile-toggle.js
@@ -1,20 +1,25 @@
 if (window.innerWidth <= 568) {
 
     $( document ).ready(function() {
-
         $(".view").click(function (event) {
             showImg();
+            changeSvg();
         });
     
         function showImg(){
             var image = $("#slideshow-1 img")
-            $(".description").toggle();
-
+        
             if(image.css('opacity') !== '1'){
+                $(".desconly").css("opacity", "0");
                 image.css("opacity", "1");
+                $("#viewIcon").css("display", "none");
+                $("#hideIcon").css("display", "block");
             }
             else{
+                $(".desconly").css("opacity", "1");
                 image.css("opacity", "0.1");
+                $("#viewIcon").css("display", "block");
+                $("#hideIcon").css("display", "none");
             };
 
         }
