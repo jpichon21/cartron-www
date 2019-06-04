8c0fcd1022e736192bd93269a461e20120dff045
jpichon21 <juliencfpichon@gmail.com>
2019-06-04 15:10:18 +0200

    modif layout page degustation

---
+++ web/site/js/degustation.js
@@ -0,0 +1,67 @@
+/************************************************************************************************************************/
+/************************************************************************************************************************/
+/************************************************************************************************************************/
+
+// New Slider Degustation
+$( document ).ready(function() {
+
+    var positionx = 2;
+    var is_moving = false;
+    screen_width = $(window).width();
+    var bg = false;
+
+    this.move = function(){		
+    var coef = 4;
+        bg.mousemove(function(e){
+            if( is_moving ) return;
+            var offset = ((e.pageX/screen_width - 0.5) * 2 *coef);
+            bg.css({'background-position': (positionx+offset/2)+'% 50%' });
+            overlay.css({'margin-left': coef*offset+'px'});
+
+        });
+    }
+    
+    $(".degustation-home .content a.next, .degustation-home .content a.prev").click(function() {
+  
+        console.log('clicked');
+        console.log(positionx);
+
+        var goto_left = $(this).hasClass('next'); 
+
+        if( goto_left ) positionx += 48;
+        else positionx -= 48;
+        
+        /* hide next icon*/
+        if( positionx >= 98 ){ $('.degustation-home .content a.next').hide(); }
+        else $('.degustation-home .content a.next').show();
+        
+        /*hide prev icon*/
+        if( positionx <= 2 ){ $('.degustation-home .content a.prev').hide(); }
+        else $('.degustation-home .content a.prev').show();
+            
+        var $active = $('.degustation-home .content .sh');
+        
+        if( goto_left ) var $next = $active.next();
+        else var $next = $active.prev();
+        
+        $active.find('h2 b').css('right', goto_left?'-100%':'100%').animate({right:100}, 800); 
+        setTimeout(function(){moveElement($next, $active, goto_left);}, 100);
+
+
+
+    });
+
+    function moveElement($next, $active, goto_left){
+        $active.removeClass('sh').addClass('hd'); 			
+        $next.addClass('sh').find('h2 b').css('left', goto_left?'100%':'-100%').animate({left:0}, 800); 
+    };
+
+    is_moving = true;
+    var coef = 4;
+    var offset = ((e.clientX/screen_width - 0.5) * 2 *coef);
+    bg.animate({'backgroundPosition': (positionx+offset/2)+'% 50%' }, 800, function(){ is_moving = false; });
+
+
+ });
+
+        
--- web/site/js/main.js
+++ web/site/js/main.js
@@ -371,56 +371,6 @@ if (!Array.prototype.indexOf) {
   }
 }
 
-/************************************************************************************************************************/
-/************************************************************************************************************************/
-/************************************************************************************************************************/
 
-// New Slider Degustation
-     $( document ).ready(function() {
-
-        var positionx = 2;
-
-	    $(".degustation-home .content a.next, .degustation-home .content a.prev").click(function() {
-      
-            console.log('clicked');
-            console.log(positionx);
-
-			var goto_left = $(this).hasClass('next'); 
-  
-			if( goto_left ) positionx += 48;
-			else positionx -= 48;
-            
-            /* hide next icon*/
-			if( positionx >= 98 ){ $('.degustation-home .content a.next').hide(); }
-			else $('.degustation-home .content a.next').show();
-            
-            /*hide prev icon*/
-			if( positionx <= 2 ){ $('.degustation-home .content a.prev').hide(); }
-			else $('.degustation-home .content a.prev').show();
-			    
-			var $active = $('.degustation-home .content .sh');
-			
-			if( goto_left ) var $next = $active.next();
-			else var $next = $active.prev();
-			
-            $active.find('h2 b').css({'left' : '-100%', 'transition' : '1s ease'}); 
-            setTimeout(function(){moveElement($next, $active, goto_left);}, 400);
-
-        });
-
-        function moveElement($next, $active, goto_left){
-            $active.removeClass('sh').addClass('hd'); 			
-            $next.addClass('sh').find('h2 b').css('left', goto_left?'100%':'-100%').animate({left:0}, 0); 
-        };
-       
-        /*
-            this.is_moving = true;
-            var coef = 4;
-            var offset = ((e.clientX/that.screen_width - 0.5) * 2 *coef);
-            this.bg.animate({'backgroundPosition': (that.positionx+offset/2)+'% 50%' }, 800, function(){ that.is_moving = false; });*/
-
-     });
-
-			
 
 
