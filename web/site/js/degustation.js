fdf827d678a2dde189298f5dc8d8ff62975331b6
jpichon21 <juliencfpichon@gmail.com>
2019-06-04 21:24:35 +0200

    parallaxe degustation correctif final

--- web/site/js/degustation.js
+++ web/site/js/degustation.js
@@ -10,25 +10,21 @@ $( document ).ready(function() {
     screen_width = $(window).width();
     var bg = $('#degustation .main');
     var overlay = $('.degustation-home .content');
+    var coef = 4;
+    var offset = '';
 
     $(".parralax").mousemove(function(e){
-        console.log("is moving!");
-        var coef = 4;
-        var parralaxpositionx = 2;
-        if( is_moving ){
+        if( is_moving == false ){
+            console.log('test');
             var offset = ((e.pageX/screen_width - 0.5) * 2 *coef);
-            bg.css({'background-position': (parralaxpositionx+offset/2)+'% 50%' });
+            bg.css({'background-position': (positionx+offset/2)+'% 50%' });
             overlay.css({'left': coef*offset+'px'});
         }
     })
     
-    $(".degustation-home .content a.next, .degustation-home .content a.prev").click(function() {
-  
-        console.log('clicked');
-        console.log(positionx);
-
+    $(".degustation-home .content a.next, .degustation-home .content a.prev").click(function(e) {
+        is_moving = true;
         var goto_left = $(this).hasClass('next'); 
-
         if( goto_left ) positionx += 48;
         else positionx -= 48;
         
@@ -39,29 +35,25 @@ $( document ).ready(function() {
         /*hide prev icon*/
         if( positionx <= 2 ){ $('.degustation-home .content a.prev').hide(); }
         else $('.degustation-home .content a.prev').show();
-            
+
+        /*change active*/
         var $active = $('.degustation-home .content .sh');
-        
         if( goto_left ) var $next = $active.next();
         else var $next = $active.prev();
-        
         $active.find('h2 b').css('right', goto_left?'-100%':'100%').animate({right:100}, 800); 
-        setTimeout(function(){moveElement($next, $active, goto_left);}, 100);
-
-
-
+        setTimeout(function(){moveElement($next, $active, goto_left, e);}, 100);
     });
 
-    function moveElement($next, $active, goto_left){
+    function moveElement($next, $active, goto_left, e){
         $active.removeClass('sh').addClass('hd'); 			
         $next.addClass('sh').find('h2 b').css('left', goto_left?'100%':'-100%').animate({left:0}, 800); 
+        setTimeout(function(){slideBackground(e);}, 100);
     };
 
-    is_moving = true;
-    var coef = 4;
-    var offset = ((e.clientX/screen_width - 0.5) * 2 *coef);
-    bg.animate({'backgroundPosition': (positionx+offset/2)+'% 50%' }, 800, function(){ is_moving = false; });
 
+    function slideBackground(e){
+        bg.animate({'backgroundPosition': (positionx+offset/2)+'% 100%' }, 600, function(){ is_moving = false; });
+    };
 
  });
 
